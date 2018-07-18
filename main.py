38c1d6df9bb1b9c7b3dd7ca16686e1deb94d2810
Saul Yeaton <saul@metismachine.com>
2018-07-18 14:40:14 -0400

    commit changes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #   Here we provide a template to precit bitcoin price and deploy   #
 #   at scale on a user-defined schedule, taking advantage of        #
 #   Metis Machine curated data, and external 3rd party data.        #
+# Test comment #1 ...
+
+
 
 ## Import some needed dependencies
 import os
